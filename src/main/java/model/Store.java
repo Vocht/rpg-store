9102f6e1f64dd03a1ad662cf37be1b521aa22f64
Vocht <jorisrenting@gmail.com>
2022-09-17 12:18:58 +0200

    We also need a store, and a store also needs an inventory.
    We notice that at this point, we have 2 classes, which both have a "getInventory" method. This can be considered a pattern. At this point, we can use our tests to make sure we can refactor without regression while implementing an interface for objects which have a "getInventory" method.

---
+++ src/main/java/model/Store.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Store {
+    public Inventory getInventory() {
+        return new Inventory();
+    }
+}
---
+++ src/test/java/model/StoreTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class StoreTest {
+
+    private Store store;
+
+    @BeforeEach
+    public void SetUp() {
+        store = new Store();
+    }
+
+    @Test
+    void store_shouldHaveInventory() {
+        var inventory = store.getInventory();
+        assertNotNull(inventory);
+    }
+}
